a694e5ee630e90686eca540f37fe793cdb91890f
AmirDamavandi <amirromanista2004@gmail.com>
2025-03-03 13:01:56 +0330

    created Artist model

--- artists/admin.py
+++ artists/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from artists.models import Artist
+from users.admin import UserCreationForm, UserChangeForm, UserAdmin as ArtistAdmin
+
 # Register your models here.
+
+
+admin.site.register(Artist, ArtistAdmin)
--- artists/models.py
+++ artists/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from users.models import User
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+
+class Artist(User):
+
+    class Meta:
+        verbose_name = _('Artist')
+        verbose_name_plural = _('Artists')

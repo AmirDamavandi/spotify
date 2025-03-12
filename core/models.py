dda609173fa37d08afd215b32c3c7d0fd8e6dcc5
AmirDamavandi <amirromanista2004@gmail.com>
2025-03-12 15:21:31 +0330

    added section model for populars

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+import uuid
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+
+class Section(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.slug
+
+    class Meta:
+        verbose_name = _('Section')
+        verbose_name_plural = _('Sections')
